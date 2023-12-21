b7210be67ff61ff3cdcfc3b3d9a7e7a3cf79c2e3
laraschke <l.raschke@chainsulting.de>
2023-12-21 13:33:06 +0100

    Change: Mumbai to deployment network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -14,76 +14,8 @@ const config: HardhatUserConfig = {
     },
   },
   networks: {
-    ethereum: {
-      url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts: [process.env.PRIVATE_KEY || ""],
-    },
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts: [process.env.PRIVATE_KEY || ""],
-    },
-    linea: {
-      url: `https://linea-goerli.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts: [process.env.PRIVATE_KEY || ""],
-    },
-    lineaGoerli: {
-      url: `https://linea-mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts: [process.env.PRIVATE_KEY || ""],
-    },
-    polygon: {
-      url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts: [process.env.PRIVATE_KEY || ""],
-    },
-    polygonMumbai: {
+    mumbai: {
       url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts: [process.env.PRIVATE_KEY || ""],
-    },
-    base: {
-      url: `https://base-mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts: [process.env.PRIVATE_KEY || ""],
-    },
-    baseSepolia: {
-      url: `https://base-sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts: [process.env.PRIVATE_KEY || ""],
-    },
-    optimism: {
-      url: `https://optimism-mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts: [process.env.PRIVATE_KEY || ""],
-    },
-    optimismSepolia: {
-      url: `https://optimism-sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts: [process.env.PRIVATE_KEY || ""],
-    },
-    arbitrum: {
-      url: `https://arbitrum-mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts: [process.env.PRIVATE_KEY || ""],
-    },
-    arbitrumSepolia: {
-      url: `https://arbitrum-sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts: [process.env.PRIVATE_KEY || ""],
-    },
-    avalanche: {
-      url: `https://avalanche-mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts: [process.env.PRIVATE_KEY || ""],
-    },
-    avalancheFuji: {
-      url: `https://avalanche-fuji.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts: [process.env.PRIVATE_KEY || ""],
-    },
-    starknet: {
-      url: `https://starknet-mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts: [process.env.PRIVATE_KEY || ""],
-    },
-    starknetSepolia: {
-      url: `https://starknet-sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts: [process.env.PRIVATE_KEY || ""],
-    },
-    bnb: {
-      url: `https://bnbsmartchain-mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts: [process.env.PRIVATE_KEY || ""],
-    },
-    bnbTestnet: {
-      url: `https://bnbsmartchain-testnet.infura.io/v3/${process.env.INFURA_KEY}`,
       accounts: [process.env.PRIVATE_KEY || ""],
     },
   },
