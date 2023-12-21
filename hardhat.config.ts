279458b9f87627790defbf201eb7dc732d78433e
laraschke <l.raschke@chainsulting.de>
2023-12-21 11:21:11 +0100

    Add: Deployment script, networks and config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,92 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.21",
+  solidity: {
+    version: "0.8.21",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    ethereum: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    linea: {
+      url: `https://linea-goerli.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    lineaGoerli: {
+      url: `https://linea-mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    polygon: {
+      url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    polygonMumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    base: {
+      url: `https://base-mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    baseSepolia: {
+      url: `https://base-sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    optimism: {
+      url: `https://optimism-mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    optimismSepolia: {
+      url: `https://optimism-sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    arbitrum: {
+      url: `https://arbitrum-mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    arbitrumSepolia: {
+      url: `https://arbitrum-sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    avalanche: {
+      url: `https://avalanche-mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    avalancheFuji: {
+      url: `https://avalanche-fuji.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    starknet: {
+      url: `https://starknet-mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    starknetSepolia: {
+      url: `https://starknet-sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    bnb: {
+      url: `https://bnbsmartchain-mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    bnbTestnet: {
+      url: `https://bnbsmartchain-testnet.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,20 @@
 import { ethers } from "hardhat";
 
-async function main() {}
+async function main() {
+  // get deployer information
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log(
+    "Account balance:",
+    (await deployer.provider.getBalance(deployer.address)).toString()
+  );
+
+  // deploy token contract
+  const Token = await ethers.getContractFactory("ERC20Demo");
+  const token = await Token.deploy(deployer.address);
+  await token.waitForDeployment();
+  console.log("Token deployed at:", token.target);
+}
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
