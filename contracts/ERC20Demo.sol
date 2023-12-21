7d91c1f9ce5bc6f532665bb1f7ddc3728b1b5e5b
laraschke <l.raschke@chainsulting.de>
2023-12-21 10:48:37 +0100

    Add: Inline Documentation

--- contracts/ERC20Demo.sol
+++ contracts/ERC20Demo.sol
@@ -7,13 +7,37 @@ import {Ownable2Step, Ownable} from "@openzeppelin/contracts/access/Ownable2Step
 
 import {IERC20Demo} from "./IERC20Demo.sol";
 
+/**
+ * @dev Implementation of the {IERC20Demo} interface.
+ *
+ * This implementation is inheritted from the {ERC20} implementation with the addition of a `mint` function that
+ * increases the `totalSupply` of the token and the {ERC20Burnable} extension that allows token holders to destroy
+ * their tokens. The {ERC20} implementation includes all function defined in EIP20. 
+ 
+ * The maximum total supply that can ever exist is restricted by `MAX_TOTAL_SUPPLY` and minting is restricted to
+ * the `owner` managed by OpenZeppelins {Ownable2Step}.
+ */
 contract ERC20Demo is ERC20, ERC20Burnable, Ownable2Step, IERC20Demo {
     uint256 public constant MAX_TOTAL_SUPPLY = 1_000_000 * (10 ** 18);
 
+    /**
+     * @dev Initializes the contract by setting a `name` and a `symbol` to the token collection
+     * and setting the `initialOwner` as the `owner` address that is allowed to mint tokens.
+     * @param initialOwner The address of the initial `owner` of the token contract.
+     */
     constructor(
         address initialOwner
     ) ERC20("DemoToken", "DMT") Ownable(initialOwner) {}
 
+    /**
+     * @dev See {IERC20Demo-mint}.
+     *
+     * Requirements:
+     *
+     * - `to` cannot be the zero address.
+     * - the caller must be the `owner`.
+     * - the `totalSupply` plus `amount` must not be greater than the `MAX_TOTAL_SUPPLY`.
+     */
     function mint(address to, uint256 amount) external onlyOwner {
         // Check if total supply will exceed the maximum total supply
         if (totalSupply() + amount > MAX_TOTAL_SUPPLY) {
--- contracts/IERC20Demo.sol
+++ contracts/IERC20Demo.sol
@@ -3,11 +3,27 @@ pragma solidity 0.8.21;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+/**
+ * @dev Interface of the ERC20Demo
+ */
 interface IERC20Demo is IERC20 {
+    /**
+     * @dev Thrown on mintin if the maximum total supply is exceeded.
+     */
     error MaxTotalSupplyExceeded(
         uint256 exceededTotalSupply,
         uint256 maxTotalSupply
     );
 
+    /**
+     * @dev Creates an `amount` amount of tokens and assigns them to `to`, by transferring it from address(0).
+     * This function is restricted to the `owner` managed by OpenZeppelins Ownable2Step. The function increases
+     * the `totalSupply` of the token by the given `amount`. The maximum total supply is restricted by
+     * `MAX_TOTAL_SUPPLY` and checked before minting to prevent minting more tokens than allowed.
+     * @param to The address of the recipient of the minted tokens.
+     * @param amount The amount of tokens to mint.
+     *
+     * Emits a {Transfer} event with `from` set to the zero address.
+     */
     function mint(address to, uint256 amount) external;
 }
